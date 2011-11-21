4af0dab9297792887dac7e32b5e7758b1f5c7b7c
Alex Demchenko <pilot@pesochin.net>
2011-11-21 16:02:49 +0200

    project clean up

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -14,30 +14,16 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Symfony\Bundle\DoctrineFixturesBundle\DoctrineFixturesBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            
-            // To remove
-            new Acme\DemoBundle\AcmeDemoBundle(),
-            
-            // Qa bundles
-            new Qa\UserBundle\QaUserBundle(),
-            new Qa\QuestionBundle\QaQuestionBundle(),
-            new Qa\DisqusBundle\QaDisqusBundle(),
-            new Qa\PaginatorBundle\QaPaginatorBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Symfony\Bundle\WebConfiguratorBundle\SymfonyWebConfiguratorBundle();
-            $bundles[] = new Behat\BehatBundle\BehatBundle();
-            $bundles[] = new Behat\MinkBundle\BehatMinkBundle();
-
-			// include PHPUnit assertions
-			require_once 'PHPUnit/Autoload.php';
-			require_once 'PHPUnit/Framework/Assert/Functions.php';
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return $bundles;
